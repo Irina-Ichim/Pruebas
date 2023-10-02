4ec1b0a0ad1ba899f0b7a7ea29e17ee759715e91
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-03 01:55:24 +0200

    ornenando las pruebas, actualizando archivos html, css y js para la galeria con three.js

--- js/prueba.js
+++ js/prueba.js
@@ -1,12 +1,15 @@
-let degrees = 0;
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
 let box = document.querySelector('.box');
 
-document.querySelector('.prev').addEventListener('click', function(){
-    degrees += 45;
-    box.style = `transform: perspective(1000px) rotateY(${degrees}deg)`;
+let degrees = 0;
+
+prev.addEventListener('click', function(){
+    degrees -= 45; // Cambié el signo para que rote hacia la izquierda
+    box.style.transform = `perspective(1000px) rotateY(${degrees}deg)`;
 });
 
-document.querySelector('.next').addEventListener('click', function(){
-    degrees += 45;
-    box.style = `transform: perspective(1000px) rotateY(${degrees}deg)`;
+next.addEventListener('click', function(){
+    degrees += 45; // Mantiene el signo para rotar hacia la derecha
+    box.style.transform = `perspective(1000px) rotateY(${degrees}deg)`;
 });
